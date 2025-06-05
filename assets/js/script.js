add4c5d9cfb4e1fc64328b170477aea7af5ed98b
Athersh-SR <athersh946@gmail.com>
2025-06-05 14:17:00 +0530

    sixth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
- $(document).ready(function () {
-    $('.nav-links li').click(function () {
-      $('.nav-links li').removeClass('active'); 
-      $(this).addClass('active');
+$(document).ready(function () {
+    $('.nav-links .nav-item a').on('click', function () {
+      $('.nav-links .nav-item').removeClass('active');
+      $(this).parent().addClass('active');
     });
   });
+ const header = document.querySelector(".header");
+  let showingSecond = false;
+
+  setInterval(() => {
+    showingSecond = !showingSecond;
+    header.classList.toggle("show-second", showingSecond);
+  }, 5000); 
